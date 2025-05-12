7f28ccbf99936c128d5b6871cbef391d601a8956
Elie Carlos <ecc226@nau.edu>
2025-05-12 23:50:35 +0000

    Chapter 2  Video 11

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,24 @@
 #include <cstdint>
 
 int main(){
-    
+    int fahrenheit = 100;
+    int celsius;
+
+    // celsius = (5 / 9) * (fahrenheit - 32);
+    celsius = ((float)5 / 9.0) * (fahrenheit - 32);
+
+    std::cout << "Fahreinheit: " << fahrenheit << std::endl;
+    std::cout << "Celsius: " << celsius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Float          : " << weight << std::endl;
+    std::cout << "Integer part   : " << (int)weight << std::endl;
+    std::cout << "Fractional part:: " << (int)((weight - (int)weight) * 10000) << std::endl;
+
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
