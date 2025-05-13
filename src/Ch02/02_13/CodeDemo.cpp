518af8ac724a9f1054096ea2603075161d561dab
Elie Carlos <ecc226@nau.edu>
2025-05-13 00:11:51 +0000

    Chapter 2 Challenge Attempt

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -8,7 +8,8 @@ int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
-    // Write your code here
+    float sum = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+    result = sum / 5.0;
     
     std::cout << "The average is " << result << std::endl;
     
